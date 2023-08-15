fd68f173f2b5dbbbba8aab77abd0b5898b340bb7
undancer <undancer+github@gmail.com>
2023-08-15 17:02:29 +0800

    修改脚本

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify/functions";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import { loadEnv } from "vite";
+const { SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://example.com',
+  // site: "https://example.com",
+  site: SITE,
   integrations: [mdx(), sitemap(), vue(), tailwind(), image()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
