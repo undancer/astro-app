970dd9d24f278f0045b9eba0fe18364b8c2912d9
undancer <undancer+github@gmail.com>
2023-08-15 16:34:47 +0800

    添加依赖

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import netlify from "@astrojs/netlify/functions";
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  // site: 'https://example.com',
+  integrations: [mdx(), sitemap(), vue(), tailwind(), image()],
+  output: "server",
+  adapter: netlify()
 });
