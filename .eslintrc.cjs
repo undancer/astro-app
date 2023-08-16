da432103c7a7d76a109046b4df8af6d1ad5c167c
undancer <undancer+github@gmail.com>
2023-08-16 09:50:54 +0800

    暂存

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+    extends: [
+        '@antfu',
+    ],
+    overrides: [{
+        files: ['*.cjs','*.mjs', '*.js', '*.ts', '*.vue'],
+        rules: {
+            'no-console': ['off'],
+        },
+    }],
+}
