6cb38996bbfa1783c62f0fe41892f7a3a9120465
undancer <undancer+github@gmail.com>
2023-08-20 13:07:02 +0800

    暂存代码

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,18 @@
-import { cwd, env } from 'node:process'
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import netlify from '@astrojs/netlify/functions'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
 import { loadEnv } from 'vite'
 
-const { SITE } = loadEnv(env.NODE_ENV, cwd(), '')
+const { SITE } = loadEnv(process.env.NODE_ENV, process.cwd(), '')
 
 // https://astro.build/config
 export default defineConfig({
   // site: "https://example.com",
   site: SITE,
-  integrations: [mdx(), sitemap(), vue(), tailwind(), image()],
+  integrations: [mdx(), sitemap(), vue(), tailwind()],
   output: 'server',
   adapter: netlify(),
 })
