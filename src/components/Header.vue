50c6a5c3a5f62be8094fbac06fab0ac760b55c0b
undancer <undancer+github@gmail.com>
2023-08-21 21:46:40 +0800

    格式化代码

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,9 +3,9 @@
 </script>
 
 <template>
-<div>
-  header
-</div>
+  <div>
+    header
+  </div>
 </template>
 
 <style scoped>
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,15 @@
 import { defineCollection, z } from 'astro:content'
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: ({ image }) => z.object({
+const blog = defineCollection<any>({
+  // Type-check front matter using a schema
+  schema: z.object({
     title: z.string(),
     description: z.string(),
     // Transform string to Date object
-    pubDate: z
-      .string()
+    pubDate: z.string()
       .or(z.date())
       .transform(val => new Date(val)),
-    updatedDate: z
-      .string()
+    updatedDate: z.string()
       .optional()
       .transform(str => (str ? new Date(str) : undefined)),
     // heroImage: z.string().optional(),
