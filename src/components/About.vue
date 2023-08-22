aed8f1d8107d3d4e4f2c24dda3e86d6df2020582
undancer <undancer+github@gmail.com>
2023-08-22 12:21:11 +0800

    提交测试代码

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+
+console.log("about")
+
+const count = ref(0)
+
+</script>
+
+<template>
+<div>
+  about
+  <button type="button" @click="count++">{{count}}</button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
